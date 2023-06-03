32facfe4821e767b45939dd55b92b17d9106f924
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-06-03 15:32:36 +0200

    Add searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Searchbar } from "./Searchbar/Searchbar";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,9 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+
+      <Searchbar/>
+   
     </div>
   );
 };
